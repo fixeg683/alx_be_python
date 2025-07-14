c3d6ea5b5e055fdf63f0483d4dbaf58fb3c8caa0
OtanaJWilson <78755889+fixeg683@users.noreply.github.com>
2025-07-14 10:40:19 +0300

    Update match_case_calculator.py

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -23,3 +23,8 @@ try:
                 result = num1 / num2
                 print(f"The result is {result}.")
             else:
+                print("Cannot divide by zero.")
+        case _:
+            print("Invalid operation selected.")
+except ValueError:
+    print("Invalid input. Please enter valid numbers.")
